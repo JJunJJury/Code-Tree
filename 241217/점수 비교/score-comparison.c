9f329c9c397086990768059fbc9e2951a524411f
JJunJJury <qaz4324152@naver.com>
2024-12-17 20:48:55 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241217/점수 비교/score-comparison.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a_m,a_e,b_m,b_e;
+    scanf("%d %d",&a_m,&a_e);
+    scanf("%d %d",&b_m,&b_e);
+    if(a_m>b_m&&a_e>b_e)
+        printf("1\n");
+    else
+        printf("0\n");
+
+    return 0;
+}
