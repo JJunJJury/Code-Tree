fcda96c3136c43eca435a15824a437dd7e4b6ae0
JJunJJury <qaz4324152@naver.com>
2024-12-18 23:37:19 +0900

    Title: 완전수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241218/완전수 판별기/perfect-number-discriminator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n,sum=0;
+    scanf("%d",&n);
+    for(int i=1;i<n;i++)
+    {
+        if(n%i==0)
+            sum+=i;
+    }
+    if(n==sum)
+        printf("P");
+    else
+        printf("N");
+    return 0;
+}
