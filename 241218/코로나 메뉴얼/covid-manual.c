bcb3f755fa2da8e4b9a21a7f1e368f3413436e16
JJunJJury <qaz4324152@naver.com>
2024-12-18 11:36:59 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241218/코로나 메뉴얼/covid-manual.c
+++ 241218/코로나 메뉴얼/covid-manual.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int temp[3],count;
-    char yn[3];
-    count=0;
+    int temp[50],count=0;
+    char yn[50];
     for(int i=0;i<3;i++)
     {
-        scanf("%c %d",&yn[i],&temp[i]);
+        scanf(" %c %d",&yn[i],&temp[i]);
         if(yn[i]=='Y'&&temp[i]>=37)
         {
             count++;
         }
     }
-    count++;
     if(count>=2)
         printf("E");
     else
