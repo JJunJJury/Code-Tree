72c861206031012cc3ef7ad84e1baaefc5552726
JJunJJury <qaz4324152@naver.com>
2024-12-23 14:50:45 +0900

    Title: 단어로 구분하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241223/단어로 구분하기/separate-words-with-words.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char a[10][200];
+    for(int i=0;i<10;i++)
+    {
+        scanf("%s",a[i]);
+    }
+    for(int i=0;i<10;i++)
+    {
+        printf("%s\n",a[i]);
+    }
+    return 0;
+}
