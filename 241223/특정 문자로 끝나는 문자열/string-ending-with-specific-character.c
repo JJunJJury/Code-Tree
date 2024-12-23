df0dfab93450f1d5a3364a7595b99bcab40211b1
JJunJJury <qaz4324152@naver.com>
2024-12-23 15:27:46 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241223/특정 문자로 끝나는 문자열/string-ending-with-specific-character.c
+++ 241223/특정 문자로 끝나는 문자열/string-ending-with-specific-character.c
@@ -3,7 +3,7 @@
 
 int main() {
     char a[10][20],word;
-    int len[10];
+    int len[10],num=0;
     for(int i=0;i<10;i++)
     {
         scanf("%s",a[i]);
@@ -15,8 +15,11 @@ int main() {
     { 
         if(a[i][len[i]-1]==word)
         {
+            num=1;
             printf("%s\n",a[i]);
         }
     }
+    if(num==0)
+        printf("None");
     return 0;
 }
