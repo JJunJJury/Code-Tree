a9f584031cdae4a0c3965b754494a8bbbdcb2ed6
JJunJJury <qaz4324152@naver.com>
2024-12-25 22:19:59 +0900

    Title: 문자열 계속 지우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241225/문자열 계속 지우기/keep-removing-string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char a[100],b[100];
+    int site;
+    scanf("%s",a);
+    scanf("%s",b);
+    int lentha=strlen(a);
+    int lenthb=strlen(b);
+    for(int i=0;i<lentha-lenthb;i++)
+    {
+        for(int j=i;j<i+lenthb;j++)
+        {
+            
+        }
+    }
+
+    return 0;
+}
