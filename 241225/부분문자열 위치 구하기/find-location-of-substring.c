d29c29fe9a2fa2a50f62baa81cd11e9ee5af0327
JJunJJury <qaz4324152@naver.com>
2024-12-25 14:44:14 +0900

    Title: 부분문자열 위치 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241225/부분문자열 위치 구하기/find-location-of-substring.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char a[1000],b[1000];
+    int check=0,check2=0,lenth,checklen,site;
+    scanf("%s",a);
+    scanf("%s",b);
+    lenth=strlen(a);
+    checklen=strlen(b);
+    for(int i=0;i<lenth-checklen;i++)
+    {
+        check=0;
+        for(int j=i;j<i+checklen;j++)
+        {
+            if(check==0&&a[j]!=b[j-i])
+            {
+                check=1;
+            }
+        }
+        if(check==0&&check2==0)
+        {
+            site=i;
+            check2=1;
+        }
+    }
+    printf("%d",site);
+    return 0;
+}
