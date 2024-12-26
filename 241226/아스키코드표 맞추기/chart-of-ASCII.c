505e7d1590b6b3fc6a66a003da83c7c5549d9c04
JJunJJury <qaz4324152@naver.com>
2024-12-26 16:40:44 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241226/아스키코드표 맞추기/chart-of-ASCII.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    char a,b,c,d,e;
+    scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+    printf("%c %c %c %c %c",(char)a,(char)b,(char)c,(char)d,(char)e);
+    return 0;
+}
