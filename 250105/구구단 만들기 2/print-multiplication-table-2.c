b172530feb4c9b1173569453d11b794841a55a9c
JJunJJury <qaz4324152@naver.com>
2025-01-05 00:12:59 +0900

    Title: 구구단 만들기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250105/구구단 만들기 2/print-multiplication-table-2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    for(int i=2;i<=8;i+=2)
+    {
+        for(int j=b;j>=a;j--)
+        {
+            printf("%d * %d = %d",j,i,j*i);
+            if(j!=a)
+                printf(" / ");
+        }
+        puts("");
+    }
+    return 0;
+}
