7538a5477f370e6e60d37f05bb9f7186041ada1d
JJunJJury <qaz4324152@naver.com>
2025-01-11 23:20:11 +0900

    Title: 최대공약수 구하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250111/최대공약수 구하기/find-the-greatest-common-divisor.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int Findmax(int n,int m);
+int main() {
+    int n,m;
+    scanf("%d %d",&n,&m);
+    printf("%d",Findmax(n,m));
+    return 0;
+}
+
+int Findmax(int n,int m)
+{
+    int max=0;
+    if(n>=m)
+    {
+        for(int i=1;i<n;i++)
+        {
+            if(n%i==0&&m%i==0)
+            {
+                if(i>max)
+                    max=i;
+            }
+        }
+    }
+    else
+    {
+        for(int i=1;i<m;i++)
+        {
+            if(n%i==0&&m%i==0)
+            {
+                if(i>max)
+                    max=i;
+            }
+        }
+    }
+    return max;
+}
