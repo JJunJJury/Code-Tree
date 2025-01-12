e0840d5db48c6c6faa61f08b6564e8d49ecc11a2
JJunJJury <qaz4324152@naver.com>
2025-01-12 19:53:44 +0900

    Title: 사칙연산 함수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250112/사칙연산 함수/quadratic-operations-function.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int sum(int a,int b);
+int sub(int a,int b);
+int mul(int a,int b);
+int div(int a,int b);
+int main() {
+    int a,b,num;
+    char c;
+    scanf("%d %c %d",&a,&c,&b);
+    if(c=='+')
+        num=sum(a,b);
+    else if(c=='-')
+        num=sub(a,b);
+    else if(c=='*')
+        num=mul(a,b);
+    else if(c=='/')
+        num=div(a,b);
+    else
+    {
+        printf("False");
+        return 0;
+    }
+    printf("%d %c %d = %d",a,c,b,num);
+    return 0;
+}
+
+
+int sum(int a,int b)
+{
+    return a+b;
+}
+
+int sub(int a,int b)
+{
+    return a>b?a-b:b-a;
+}
+
+int mul(int a,int b)
+{
+    return a*b;
+}
+
+int div(int a,int b)
+{
+    return a/b;
+}
