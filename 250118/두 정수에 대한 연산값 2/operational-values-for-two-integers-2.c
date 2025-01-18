6a912b2388c4bc71f2a704f95d7e762657ed641a
JJunJJury <qaz4324152@naver.com>
2025-01-18 22:58:00 +0900

    Title: 두 정수에 대한 연산값 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250118/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void Cal(int *a,int *b);
+int main() {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    Cal(&a,&b);
+    printf("%d %d",a,b);
+    return 0;
+}
+
+void Cal(int *a,int *b)
+{
+    if(a>b)
+    {
+        *a*=2;
+        *b+=10;
+    }
+    else
+    {
+        *a+=10;
+        *b*=2;
+    }
+}
