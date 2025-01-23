dbb76efa70441d2576d48025d31d158cc3da20d9
JJunJJury <qaz4324152@naver.com>
2025-01-23 21:18:56 +0900

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250123/요일 맞추기/guess-day-of-week.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+int main() {
+    int mon[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+    int m1,d1,m2,d2,day=0,date,month,when=1;
+    scanf("%d %d %d %d",&m1,&d1,&m2,&d2);
+    if(m2<m1||(m1==m2&&d2<d1))
+    {
+        date=d2;
+        month=m2;
+        while(1)
+        {
+            if(month==m1&&date==d1)
+                break;
+            date++;
+            day++;
+            if(date>mon[month-1])  
+            {
+                date=1;
+                month++;
+            }
+        }
+        day*=-1;
+    }
+    else
+    {
+        date=d1;
+        month=m1;
+        while(1)
+        {
+            if(month==m2&&date==d2)
+                break;
+
+            date++;
+            day++;
+            if(date>mon[month-1])  
+            {
+                date=1;
+                month++;
+            }
+        }
+    }
+    when+=day;
+    when%=7;
+    switch(when)
+    {
+        case 0:
+            printf("Sun");
+            break;
+        case 1:
+            printf("Mon");
+            break;
+        case 2:
+            printf("Tue");
+            break;
+        case 3:
+            printf("Wed");
+            break;
+        case 4:
+            printf("Thu");
+            break;
+        case 5:
+            printf("Fri");
+            break;
+        case 6:
+            printf("Sat");
+            break;
+        default:
+            break;
+    }
+    return 0;
+}
