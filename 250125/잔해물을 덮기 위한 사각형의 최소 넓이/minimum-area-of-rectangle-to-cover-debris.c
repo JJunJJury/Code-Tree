bdc88467322e6847328f1050ba5390c3d8e2c3ac
JJunJJury <qaz4324152@naver.com>
2025-01-25 17:21:45 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 19ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250125/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int a[2000][2000]={0,},x1,y1,x2,y2,x_min=2000,x_max=0,y_min=2000,y_max=0;
+    scanf("%d %d %d %d",&x1,&y1,&x2,&y2);
+    for(int i=x1+1000;i<x2+1000;i++)
+    {
+        for(int j=y1+1000;j<y2+1000;j++)
+        {
+            a[i][j]++;
+        }
+    }
+    scanf("%d %d %d %d",&x1,&y1,&x2,&y2);
+    for(int i=x1+1000;i<x2+1000;i++)
+    {
+        for(int j=y1+1000;j<y2+1000;j++)
+        {
+            if(a[i][j]==1)
+                a[i][j]--;
+        }
+    }
+    for(int i=0;i<2000;i++)
+    {
+        for(int j=0;j<2000;j++)
+        {
+            if(a[i][j]==1)
+            {
+                if(i<x_min)
+                    x_min=i;
+                if(i>x_max)
+                    x_max=i+1;
+                if(j<y_min)
+                    y_min=j;
+                if(j>y_max)
+                    y_max=j+1;
+            }
+        }
+    }
+    printf("%d",(x_max-x_min)*(y_max-y_min));
+    return 0;
+}
