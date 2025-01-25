0a20cf20c96a996483837d91bfaa6ed748d3a8c3
JJunJJury <qaz4324152@naver.com>
2025-01-25 20:17:19 +0900

    Title: 만나는 그 순간, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 250125/만나는 그 순간/the-moment-we-meet.c
+++ 250125/만나는 그 순간/the-moment-we-meet.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int a[1000]={0,},b[1000]={0,},n,m,t,loc=1,meet=-1,check=0;
+    int a[1000]={2000,},b[1000]={2000,},n,m,t,loc=1,meet=-1,check=0;
+    for(int i=0;i<1000;i++)
+    {
+        a[i]=2000;
+        b[i]=2000;
+    }
+    a[0]=0,b[0]=0;
     char direction;
     scanf("%d %d",&n,&m);
     for(int i=0;i<n;i++)
@@ -47,7 +53,7 @@ int main() {
     }
     for(int i=1;i<1000;i++)
     {
-        if(a[i]==b[i]&&check==0){
+        if(a[i]==b[i]&&check==0&&a[i]<1500&&b[i]<1500){
             check=1;
             meet=i;
         }
