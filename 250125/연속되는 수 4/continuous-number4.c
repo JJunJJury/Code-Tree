8a0645020e24f63fdb3883e214e4d7f99e4d964f
JJunJJury <qaz4324152@naver.com>
2025-01-25 18:46:42 +0900

    Title: 연속되는 수 4, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250125/연속되는 수 4/continuous-number4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a[1005],n,max,cnt=0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        cnt++; 
+        if(i==0||a[i]<a[i-1])
+        {
+            if(cnt>max)
+            {
+                max=cnt;
+                cnt=0;
+            }
+        }
+    }
+    if(a[n-1]>a[n-2])
+        cnt++;
+    if(cnt>max)
+        max=cnt;
+    printf("%d",max);
+    return 0;
+}
