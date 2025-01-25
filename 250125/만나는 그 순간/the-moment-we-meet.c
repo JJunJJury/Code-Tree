d423017a4a726f7ed4e765ca6263f7165d7b354b
JJunJJury <qaz4324152@naver.com>
2025-01-25 20:25:07 +0900

    Title: 만나는 그 순간, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250125/만나는 그 순간/the-moment-we-meet.c
+++ 250125/만나는 그 순간/the-moment-we-meet.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int a[1000]={2000,},b[1000]={2000,},n,m,t,loc=1,meet=-1,check=0;
-    for(int i=0;i<1000;i++)
-    {
-        a[i]=2000;
-        b[i]=2000;
-    }
+    int a[1000],b[1000],n,m,t,loc=1,meet=-1,check=0;
     a[0]=0,b[0]=0;
     char direction;
     scanf("%d %d",&n,&m);
@@ -53,13 +48,15 @@ int main() {
     }
     for(int i=1;i<1000;i++)
     {
-        if(a[i]==b[i]&&check==0&&a[i]<1500&&b[i]<1500){
+        if(a[i]==b[i]&&check==0){
             check=1;
             meet=i;
         }
         if(check==1)
             break;
     }
+    if(!(a[meet]==(a[meet-1]-1)||a[meet]==(a[meet-1]+1)))
+        meet=-1;
     printf("%d",meet);
     return 0;
 }
