00e412344464cdf2caceb4dcf4d4a7066d92c947
JJunJJury <qaz4324152@naver.com>
2025-01-25 17:57:44 +0900

    Title: 연속되는 수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250125/연속되는 수 2/continuous-number2.c
+++ 250125/연속되는 수 2/continuous-number2.c
@@ -6,13 +6,13 @@ int main() {
     for(int i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
+        cnt++;
         if(i==0||a[i]!=a[i-1])
         {
             if(cnt>max)
                 max=cnt;
             cnt=0;
         }
-        cnt++;
     }
     printf("%d",max);
     return 0;
