0211ec5974ccf398cff4814558632a99f1dcd781
JJunJJury <qaz4324152@naver.com>
2025-01-27 22:28:12 +0900

    Title: 구역 청소, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250127/구역 청소/district-cleaning.c
+++ 250127/구역 청소/district-cleaning.c
@@ -4,7 +4,7 @@ int main() {
     int a,b,c,d,cnt=0;
     scanf("%d %d",&a,&b);
     scanf("%d %d",&c,&d);
-    if(c>=b||a>=d)
+    if(b<=c)
     {
         for(int i=a;i<b;i++)
         {
@@ -15,30 +15,41 @@ int main() {
             cnt++;
         }
     }
-    if(c>a&&c<=b&&d>=b)
+    else if(a<=c&&c<=b&&b<=d)
     {
         for(int i=a;i<d;i++)
         {
             cnt++;
         }
     }
-    if(c<=a&&d>=a&&d<b)
+    else if(a<=c&&d<=b)
+    {
+        for(int i=a;i<b;i++)
+        {
+            cnt++;
+        }
+    }
+    else if(d<=a)
+    {
+        for(int i=c;i<d;i++)
+        {
+            cnt++;
+        }
+        for(int i=a;i<b;i++)
+        {
+            cnt++;
+        }
+    }
+    else if(c<=a&&a<=d&&d<=b)
     {
         for(int i=c;i<b;i++)
         {
             cnt++;
         }
     }
-    if(c>=a&&d<=b)
+    else if(c<=a&&b<=d)
     {
-        for(int i=a;i<b;i++)
-        {
-            cnt++;
-        }
-    }
-    if(a>=c&&b<=d)
-    {
-        for(int i=a;i<b;i++)
+        for(int i=c;i<d;i++)
         {
             cnt++;
         }
