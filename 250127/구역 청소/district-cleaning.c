44c328adf6943c076bc3cdad9ef7f38836d70fa4
JJunJJury <qaz4324152@naver.com>
2025-01-27 22:17:40 +0900

    Title: 구역 청소, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250127/구역 청소/district-cleaning.c
+++ 250127/구역 청소/district-cleaning.c
@@ -4,45 +4,31 @@ int main() {
     int a,b,c,d,cnt=0;
     scanf("%d %d",&a,&b);
     scanf("%d %d",&c,&d);
-    for(int i=a;i<=b;i++)
+    if(c>=b||a>=d)
     {
-        cnt++;
-    }
-    for(int i=c;i<=d;i++)
-    {
-        cnt++;
-    }
-    if(c>=a&&d<=b)
-    {
-        for(int i=c;i<=d;i++)
+        for(int i=a;i<b;i++)
         {
-            cnt--;
+            cnt++;
+        }
+        for(int i=c;i<d;i++)
+        {
+            cnt++;
         }
     }
-    else if(c>=a&&d>b)
+    if(c>a&&c<=b&&d>=b)
     {
-        for(int i=c;i<=b;i++)
+        for(int i=a;i<d;i++)
         {
-            cnt--;
+            cnt++;
         }
     }
-    else if(c<a&&d==a)
-        cnt--;
-    else if(c<a&&d>a&&d<=b)
+    if(c<=a&&d>=a&&d<b)
     {
-        for(int i=a;i<=d;i++)
+        for(int i=c;i<b;i++)
         {
-            cnt--;
+            cnt++;
         }
     }
-    else if(c<a&&d>b)
-    {
-        for(int i=a;i<=b;i++)
-        {
-            cnt--;
-        }
-    }
-    cnt--;
     printf("%d",cnt);
     return 0;
 }
