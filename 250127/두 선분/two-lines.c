5eb2cf6adccd404960bd1814ea7765840e738b4d
JJunJJury <qaz4324152@naver.com>
2025-01-27 21:52:03 +0900

    Title: 두 선분, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250127/두 선분/two-lines.c
+++ 250127/두 선분/two-lines.c
@@ -3,11 +3,11 @@
 int main() {
     int x1,x2,x3,x4,check=0;
     scanf("%d %d %d %d",&x1,&x2,&x3,&x4);
-    if(x1>=x4||x2>=x3)
+    if(x1>x4||x2<x3)
         check+=1;
     if(check!=0)
-        printf("intersecting");
-    else
         printf("nonintersecting");
+    else
+        printf("intersecting");
     return 0;
 }
