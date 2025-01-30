0dcfbace7643245c3943cc778a12ca8c69e1d681
JJunJJury <qaz4324152@naver.com>
2025-01-30 20:44:22 +0900

    Title: 순 간 이 동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250130/순 간 이 동/teleportation.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int a,b,x,y,distance=0,save=INT_MAX,temp;
+    scanf("%d %d %d %d",&a,&b,&x,&y);
+    if(a>b)
+    {
+        temp=a;
+        a=b;
+        b=temp;
+    }
+    if(x>y)
+    {
+        temp=x;
+        x=y;
+        y=temp;
+    }
+    distance=0;
+    if(x<a&&a<y&&y<b)
+    {
+        distance=a-x+b-y;
+        if(distance<save)
+            save=distance;
+        distance=b-a;
+        if(distance<save)
+            save=distance;
+    }
+    distance=0;
+    if(b<x||y<a)
+    {
+        distance=b-a;
+        if(distance<save)
+            save=distance;
+    }
+    distance=0;
+    if(a<x&&x<b&&b<y)
+    {
+        distance=x-a+y-b;
+        if(distance<save)
+            save=distance;
+        distance=b-a;
+        if(distance<save)
+            save=distance;
+    }
+    distance=0;
+    if(x<a&&a<b&&b<y)
+    {
+        distance=a-b+y-b;
+        if(distance<save)
+            save=distance;
+        distance=b-a;
+        if(distance<save)
+            save=distance;
+    }
+    if(a<x&&x<y&&y<b)
+    {
+        distance=x-a+b-y;
+        if(distance<save)
+            save=distance;
+        distance=b-a;
+        if(distance<save)
+            save=distance;
+    }
+    printf("%d",save);
+    return 0;
+}
