8c0eb9abf14fe069062482ba0eaab7574e6267b8
JJunJJury <qaz4324152@naver.com>
2025-01-30 22:12:34 +0900

    Title: 연속된 숫자 만들기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250130/연속된 숫자 만들기 3/create-consecutive-numbers-3.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+void Sort(int *a,int *b,int *c);
+int main() {
+    int a,b,c,cnt=0,check=0,sum;
+    scanf("%d %d %d",&a,&b,&c);
+    if(b-a==1&&c-b==1)
+        check=1;
+    Sort(&a,&b,&c);
+    if(b-a==1&&c-b==1)
+        check=1;
+    while(!(b-a==1&&c-b==1))
+    {
+        if(b-a==2||c-b==2)
+        {
+            cnt+=1;
+            break;
+        }
+        else if(c-b<b-a)
+        {
+            c=b-1;
+            cnt++;
+        }
+        else if(c-b>b-a)
+        {
+            a=b+1;
+            cnt++;
+        }
+        Sort(&a,&b,&c);
+    }
+    if(check!=0)
+        printf("0");
+    else
+        printf("%d",cnt);
+    return 0;
+}
+
+void Sort(int *a,int *b,int *c)
+{
+    int temp;
+    if(*a>*b)
+    {
+        temp=*a;
+        *a=*b;
+        *b=temp;
+    }
+    if(*b>*c)
+    {
+        temp=*b;
+        *b=*c;
+        *c=temp;
+    }
+    if(*a>*b)
+    {
+        temp=*a;
+        *a=*b;
+        *b=temp;
+    }
+}
