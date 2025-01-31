390306d50ee892f586d73e53f07c7397f15d7552
JJunJJury <qaz4324152@naver.com>
2025-01-31 23:48:44 +0900

    Title: 두 점으로 만드는 직사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250131/두 점으로 만드는 직사각형/rectangle-made-of-two-points.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int x1,y1,x2,y2,a1,b1,a2,b2,x,y,a,b;
+    scanf("%d %d %d %d",&x1,&y1,&x2,&y2);
+    scanf("%d %d %d %d",&a1,&b1,&a2,&b2);
+    if(x1<=a1)
+        x=x1;
+    else if(a1<x1)
+        x=a1;
+    if(y1<=b1)
+        y=y1;
+    else if(b1<y1)
+        y=b1;
+    if(x2>=a2)
+        a=x2;
+    else if(a2>x2)
+        a=a2;
+    if(y2>=b2)
+        b=y2;
+    else if(b2>y2)
+        b=b2;
+    printf("%d",(a-x)*(b-y));
+    return 0;
+}
