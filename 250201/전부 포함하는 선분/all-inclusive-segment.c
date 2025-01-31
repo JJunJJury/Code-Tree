cade2220de0eca9b0eee592a6eb58aed9ea2cc3c
JJunJJury <qaz4324152@naver.com>
2025-02-01 03:14:46 +0900

    Title: 전부 포함하는 선분, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250201/전부 포함하는 선분/all-inclusive-segment.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int n,line[102]={0,},x1[102],x2[102],min=INT_MAX,cnt=0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d %d",&x1[i],&x2[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        cnt=0;
+        for(int j=0;j<n;j++)
+        {
+            if(i==j)
+                continue;
+            for(int k=x1[j];k<=x2[j];k++)
+            {
+                line[k]++;
+            }
+        }
+        for(int j=1;j<=100;j++)
+        {
+            printf("%d ",line[j]);
+            if(line[j]!=0)
+            {
+                cnt++;
+                line[j]=0;
+            }
+        }
+        if(cnt<min)
+            min=cnt;
+        puts("");
+    }
+    printf("%d",min-1);
+    return 0;
+}
