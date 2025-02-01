ab09baf75721f7524b021a7aaf8481ac3ac8ea34
JJunJJury <qaz4324152@naver.com>
2025-02-01 13:56:02 +0900

    Title: 원 모양으로 되어있는 방, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250201/원 모양으로 되어있는 방/a-room-in-a-circle.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int a[1005],n,cnt,min=INT_MAX;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        cnt=0;
+        for(int j=0;j<n;j++)
+        {
+            if(i==j)
+                continue;
+            cnt+=((j-i+n)%5)*a[j];
+        }
+        if(cnt<min)
+            min=cnt;
+    }
+    printf("%d",min);
+    return 0;
+}
