1ea2b5a517f2bd6ffd6426236bc132bac549e178
JJunJJury <qaz4324152@naver.com>
2025-02-01 14:42:59 +0900

    Title: Carry 피하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250201/Carry 피하기 2/escaping-carry-2.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main() {
+    int a[25],n,max=0,ch1,ch2,ch3,num,sum=0,check=0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n-2;i++)
+    {
+        for(int j=i+1;j<n-1;j++)
+        {
+            for(int k=j+1;k<n;k++)
+            {
+                check=0;
+                ch1=a[i];
+                ch2=a[j];
+                ch3=a[k];
+                while(1)
+                {
+                    if(ch1<10&&ch2<10&&ch3<10)
+                        break;
+                    num=(ch1%10)+(ch2%10)+(ch3%10);
+                    if(num>=10)
+                    {
+                        check=1;
+                        break;
+                    }
+                    ch1/=10;
+                    ch2/=10;
+                    ch3/=10;
+                }
+                num=ch1+ch2+ch3;
+                if(num>=10)
+                {
+                    check=1;
+                }
+                if(check==0)
+                {
+                    num=a[i]+a[j]+a[k];
+                    if(num>max)
+                        max=num;
+                }
+            }
+        }
+    }
+    printf("%d",max);
+    return 0;
+}
