e4559cbe89faf6efe00ee7b37db8287b8474a2cb
JJunJJury <qaz4324152@naver.com>
2025-02-02 15:11:16 +0900

    Title: 악수와 전염병의 상관관계 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250202/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int N,K,P,T,a[105]={0,},check[105]={0,};
+    scanf("%d %d %d %d",&N,&K,&P,&T);
+    a[P]=1;
+    for(int i=0;i<T;i++)
+    {
+        scanf("%d %d %d",&t,&x,&y);
+        
+    }
+    return 0;
+}
