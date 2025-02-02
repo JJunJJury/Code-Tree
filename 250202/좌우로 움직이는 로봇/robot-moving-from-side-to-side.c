14f7392fc839c0f183e7b6d6265a4d62237189a3
JJunJJury <qaz4324152@naver.com>
2025-02-02 15:02:57 +0900

    Title: 좌우로 움직이는 로봇, Time: 17ms, Memory: 1MB, Status: Runtime Error - CodeTree

---
+++ 250202/좌우로 움직이는 로봇/robot-moving-from-side-to-side.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int main() {
+    int a[100005]={0,},b[100005]={0,},n,m,time=0,t,cnt=0;
+    char d;
+    scanf("%d %d",&n,&m);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d %c",&t,&d);
+        if(d=='L')
+        {
+            for(int i=0;i<t;i++)
+            {
+                time++;
+                a[time]=a[time-1]-1;
+            }
+        }
+        else if(d=='R')
+        {
+            for(int j=0;j<t;j++)
+            {
+                time++;
+                a[time]=a[time-1]+1;
+            }
+        }
+    }
+    for(int i=time;i<100000;i++)
+    {
+        a[i]=a[time];
+    }
+    time=0;
+    for(int i=0;i<m;i++)
+    {
+        scanf("%d %c",&t,&d);
+        if(d=='L')
+        {
+            for(int i=0;i<t;i++)
+            {
+                time++;
+                b[time]=b[time-1]-1;
+            }
+        }
+        else if(d=='R')
+        {
+            for(int j=0;j<t;j++)
+            {
+                time++;
+                b[time]=b[time-1]+1;
+            }
+        }
+    }
+    for(int i=time;i<100000;i++)
+    {
+        b[i]=b[time];
+    }
+    for(int i=1;i<100000;i++)
+    {
+        if(a[i]==b[i]&&a[i-1]!=b[i-1])
+            cnt++;
+    }
+    printf("%d",cnt);
+    return 0;
+}
