cbfef49e4f323766c5d252b8f3f60dfb0b2ffff5
JJunJJury <qaz4324152@naver.com>
2025-02-04 18:10:53 +0900

    Title: 아름다운 수열 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250204/아름다운 수열 2/beautiful-sequence-2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a[105],b[105],cnt,n,m;
+    scanf("%d %d",&n,&m);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<m;i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    for(int i=0;i<n-m;i++)
+    {
+        for(int j=i;j<m;j++)
+        {
+            for(int k=0;k<m;k++)
+            {
+                if()
+            }
+        }
+    }
+    return 0;
+}
