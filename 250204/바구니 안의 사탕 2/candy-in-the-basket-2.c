75985a877fb9061ed94860605e580e4b9e258c22
JJunJJury <qaz4324152@naver.com>
2025-02-04 18:35:22 +0900

    Title: 바구니 안의 사탕 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250204/바구니 안의 사탕 2/candy-in-the-basket-2.c
+++ 250204/바구니 안의 사탕 2/candy-in-the-basket-2.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
 
 int main() {
-    int n,k,a[105]={0,},cnt,big=0,site,candy;
+    int n,k,a[305]={0,},cnt=0,big=0,site,candy;
     scanf("%d %d",&n,&k);
     for(int i=0;i<n;i++)
     {
         scanf("%d %d",&candy,&site);
         a[site-1]+=candy;
     }
-    for(int i=k;i<100-k;i++)
+    if(k>=50)
     {
-        cnt=0;
-        for(int j=i-k;j<=i+k;j++)
+        for(int i=0;i<100;i++)
         {
-            cnt+=a[j];
+            cnt+=a[i];
         }
         if(cnt>big)
             big=cnt;
+    }
+    else
+    {
+        for(int i=k;i<100-k;i++)
+        {
+            cnt=0;
+            for(int j=i-k;j<=i+k;j++)
+            {
+                cnt+=a[j];
+            }
+            if(cnt>big)
+                big=cnt;
+        }
     }
     printf("%d",big);
     return 0;
